ae8488ffe8d7766604e36e197878ce26fc2d4237
Douglas Gubert <douglas.gubert@gmail.com>
2023-10-04 14:00:46 -0300

    Make the DisableEslintDiagnostics function into a command

--- home/.config/nvim/lua/d-gubert/commands.lua
+++ home/.config/nvim/lua/d-gubert/commands.lua
@@ -1,1 +1,11 @@
 vim.api.nvim_command([[ autocmd BufWritePre * :%s/\s\+$//e ]])
+
+vim.api.nvim_create_user_command('DisableEslintDiagnostics', function()
+	for id, namespace in pairs(vim.diagnostic.get_namespaces()) do
+		-- for some reason this is the name of the `eslint` namespace
+		if namespace.name == 'NULL_LS_SOURCE_3' then
+			vim.diagnostic.disable(nil, id)
+		end
+	end
+end, {})
+
