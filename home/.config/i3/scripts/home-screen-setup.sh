42d88125a1f2ee9a565a33f77644f2bf61a36f59
Douglas Gubert <douglas.gubert@gmail.com>
2023-10-28 17:02:38 -0300

    Home screen setup

---
+++ home/.config/i3/scripts/home-screen-setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1920x1080 --pos 2160x1380 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --primary --mode 3840x2160 --pos 0x0 --rotate left
