f2f40c441b35cc5bed36f6f26a126f389d5257cf
Douglas Gubert <douglas.gubert@gmail.com>
2023-12-28 18:50:35 -0300

    Housekeeping

--- .config/nvim/after/plugin/hop.lua
+++ .config/nvim/after/plugin/hop.lua
@@ -1,4 +1,4 @@
 local hop = require('hop')
 
 vim.keymap.set('n', '<leader>jw', function() hop.hint_words() end, { desc = 'Hop to word' })
-vim.keymap.set('n', '<leader>jc', function() hop.hint_char1() end, { desc = 'Hop to word' })
+vim.keymap.set('n', '<leader>jc', function() hop.hint_char1() end, { desc = 'Hop to character' })
