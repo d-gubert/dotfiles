172db274c44bf883946bbdb8817d3ac276bc8881
Douglas Gubert <douglas.gubert@gmail.com>
2024-01-27 17:09:40 -0300

    Treesitter improvement on text objects, movement and stuff

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require("nvim-treesitter.configs").setup({
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = { "lua", "javascript", "typescript", "tsx", "query" },
+	ensure_installed = { "lua", "javascript", "typescript", "tsx", "query", "rust", "go" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
@@ -39,49 +39,92 @@ require("nvim-treesitter.configs").setup({
 		additional_vim_regex_highlighting = false,
 	},
 
-	-- Treesitter Text objects baby!!
 	incremental_selection = {
 		enable = true,
 		keymaps = {
-			init_selection = '<leader>t',
-			node_incremental = '<A-k>',
-			scope_incremental = '<A-K>',
-			node_decremental = '<A-j>',
+			init_selection = "<leader>t",
+			node_incremental = "<A-k>",
+			scope_incremental = "<A-K>",
+			node_decremental = "<A-j>",
 		},
 	},
 
+	indent = {
+		enable = true,
+	},
+
+	-- Treesitter Text objects baby!!
 	textobjects = {
+		lsp_interop = {
+			enable = true,
+			border = "rounded",
+			floating_preview_opts = {},
+			peek_definition_code = {
+				["<leader>pf"] = "@function.outer",
+				["<leader>pF"] = "@class.outer",
+			},
+		},
 		select = {
 			enable = true,
 			lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
 			keymaps = {
 				-- You can use the capture groups defined in textobjects.scm
-				['aa'] = '@parameter.outer',
-				['ia'] = '@parameter.inner',
-				['af'] = '@function.outer',
-				['if'] = '@function.inner',
-				['ac'] = '@class.outer',
-				['ic'] = '@class.inner',
+				["a="] = { query = "@assignment.outer", desc = "Select outer assignment" },
+				["i="] = { query = "@assignment.inner", desc = "Select inner assignment" },
+				["l="] = { query = "@assignment.lhs", desc = "Select assignment left-hand part" },
+				["r="] = { query = "@assignment.rhs", desc = "Select assignment right-hand part" },
+
+				["a:"] = { query = "@statement.outer", desc = "Select outer statement" },
+				["i:"] = { query = "@statement.inner", desc = "Select inner statement" },
+
+				["a;"] = { query = "@comment.outer", desc = "Select outer comment" },
+				["i;"] = { query = "@comment.inner", desc = "Select inner comment" },
+
+				["ai"] = { query = "@conditional.outer", desc = "Select outer conditional" },
+				["ii"] = { query = "@conditional.inner", desc = "Select inner conditional" },
+
+				["a'"] = { query = "@string.outer", desc = "Select outer string" },
+				["i'"] = { query = "@string.inner", desc = "Select inner string" },
+
+				["a{"] = { query = "@block.outer", desc = "Select outer block" },
+				["i{"] = { query = "@block.inner", desc = "Select inner block" },
+
+				["aa"] = { query = "@parameter.outer", desc = "Select outer argument/parameter" },
+				["ia"] = { query = "@parameter.inner", desc = "Select inner argument/parameter" },
+
+				["af"] = { query = "@function.outer", desc = "Select outer function body" },
+				["if"] = { query = "@function.inner", desc = "Select inner function body" },
+
+				["ac"] = { query = "@call.outer", desc = "Select outer function call" },
+				["ic"] = { query = "@call.inner", desc = "Select inner function call" },
 			},
 		},
 		move = {
 			enable = true,
 			set_jumps = true, -- whether to set jumps in the jumplist
 			goto_next_start = {
-				[']m'] = '@function.outer',
-				[']]'] = '@class.outer',
+				["]m"] = "@function.outer",
+				["]c"] = "@class.outer",
+				["]b"] = "@block.outer",
+				["]a"] = "@parameter.outer",
 			},
 			goto_next_end = {
-				[']M'] = '@function.outer',
-				[']['] = '@class.outer',
+				["]M"] = "@function.outer",
+				["]C"] = "@class.outer",
+				["]B"] = "@block.outer",
+				["]A"] = "@parameter.outer",
 			},
 			goto_previous_start = {
-				['[m'] = '@function.outer',
-				['[['] = '@class.outer',
+				["[m"] = "@function.outer",
+				["[c"] = "@class.outer",
+				["[b"] = "@block.outer",
+				["[a"] = "@parameter.outer",
 			},
 			goto_previous_end = {
-				['[M'] = '@function.outer',
-				['[]'] = '@class.outer',
+				["[M"] = "@function.outer",
+				["[C"] = "@class.outer",
+				["[B"] = "@block.outer",
+				["[A"] = "@parameter.outer",
 			},
 		},
 		-- Not sure I want this?
@@ -97,16 +140,26 @@ require("nvim-treesitter.configs").setup({
 	},
 })
 
-local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
 
 -- Repeat movement with ; and ,
 -- ensure ; goes forward and , goes backward regardless of the last direction
-vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next, { desc = "Treesitter: Repeat last move forward" })
-vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous, { desc = "Treesitter: Repeat last move backward" })
+-- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next, { desc = "Treesitter: Repeat last move forward" })
+-- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous, { desc = "Treesitter: Repeat last move backward" })
 
 -- vim way: ; goes to the direction you were moving.
--- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
--- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+vim.keymap.set(
+	{ "n", "x", "o" },
+	";",
+	ts_repeat_move.repeat_last_move,
+	{ desc = "Treesitter: Repeat last move forward" }
+)
+vim.keymap.set(
+	{ "n", "x", "o" },
+	",",
+	ts_repeat_move.repeat_last_move_opposite,
+	{ desc = "Treesitter: Repeat last move backward" }
+)
 
 -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
 vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f, { desc = "Treesitter: Repeat f movement" })
@@ -114,8 +167,8 @@ vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F, { desc = "Trees
 vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t, { desc = "Treesitter: Repeat t movement" })
 vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T, { desc = "Treesitter: Repeat T movement" })
 
-require("treesitter-context").setup()
+-- require("treesitter-context").setup()
 
-vim.keymap.set("n", "[c", function()
+vim.keymap.set("n", "[[", function()
 	require("treesitter-context").go_to_context()
 end, { silent = true, desc = "Treesitter: Go to context above" })
