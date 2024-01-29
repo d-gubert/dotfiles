6dd8c8d54c9cf30ad2e878ad2562d1da8d944224
Douglas Gubert <douglas.gubert@gmail.com>
2024-01-28 22:58:37 -0300

    [Nvim] Previous leap config didn't work

--- .config/nvim/after/plugin/leap.lua
+++ .config/nvim/after/plugin/leap.lua
@@ -5,8 +5,8 @@ if not ok then
 end
 
 -- leap.create_default_mappings()
-vim.keymap.set({ "n", "x", "o" }, "f", "<Plug>(leap-forward)", { desc = "Leap forward", noremap = true })
-vim.keymap.set({ "n", "x", "o" }, "F", "<Plug>(leap-backwards)", { desc = "Leap backward", noremap = true })
+vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)", { desc = "Leap forward", noremap = true })
+vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)", { desc = "Leap backward", noremap = true })
 vim.keymap.set({ "n", "x", "o" }, "gx", "<Plug>(leap-from-window)", { desc = "Leap from window" })
 
 -- Until https://github.com/neovim/neovim/issues/20793 is fixed, we need to
