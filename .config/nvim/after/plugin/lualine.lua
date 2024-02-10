b96f73a4b8dfb1941e7db4d2dcf0d22fe0162784
Douglas Gubert <douglas.gubert@gmail.com>
2024-02-09 21:47:43 -0300

    Show window index in lualine, to use with <C-W> w

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -8,16 +8,26 @@ lualine.setup({
 	sections = {
 		lualine_c = {
 			{
+				"windows",
+				mode = 1, -- Show window index only
+				show_modified_status = false,
+			},
+			{
 				"filename",
-				path = 1,
+				path = 1, -- Show relative path
 			},
 		},
 	},
 	inactive_sections = {
 		lualine_c = {
 			{
+				"windows",
+				mode = 1, -- Show window index only
+				show_modified_status = false,
+			},
+			{
 				"filename",
-				path = 1,
+				path = 1, -- Show relative path
 			},
 		},
 	},
