eeddad86730485ed59bb17576f3552793502a527
Douglas Gubert <douglas.gubert@gmail.com>
2024-03-07 12:42:16 -0300

    Better session management to integrate with tmux-ressurect

--- .config/nvim/after/plugin/sessions.lua
+++ .config/nvim/after/plugin/sessions.lua
@@ -1,4 +1,26 @@
-require("sessions").setup({
-    session_filepath = vim.fn.stdpath("data") .. "/sessions",
-    absolute = true,
+local ok, sessions = pcall(require, "sessions")
+
+if not ok then
+	return
+end
+
+sessions.setup({
+	session_filepath = vim.fn.stdpath("data") .. "/sessions",
+	absolute = true,
 })
+
+vim.keymap.set(
+	"n",
+	"<leader>ms",
+	"<cmd>SessionsSave<CR>",
+	{ desc = "Save session (start up autosave)", noremap = true, silent = true }
+)
+vim.keymap.set(
+	"n",
+	"<leader>ml",
+	"<cmd>SessionsLoad<CR>",
+	{ desc = "Load session (start up autosave)", noremap = true, silent = true }
+)
+vim.keymap.set("n", "<leader>ma", function()
+	sessions.start_autosave()
+end, { desc = "Start up session autosave", noremap = true, silent = true })
