3c3449bf08bae87f73d5193e1f3991655232bda6
Douglas Gubert <douglas.gubert@gmail.com>
2024-09-22 23:06:30 -0300

    Noice.nvim configuration

--- .config/nvim/lua/d-gubert/packer.lua
+++ .config/nvim/lua/d-gubert/packer.lua
@@ -68,9 +68,19 @@ return require("packer").startup(function(use)
 		"folke/noice.nvim",
 		requires = {
 			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
 		},
 		config = function()
-			require("noice").setup()
+			require("noice").setup({
+				lsp = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+					},
+				},
+			})
 		end,
 	})
 
