52047131f4a6813a3b5f02b2b372f65d899a9a90
Douglas Gubert <douglas.gubert@gmail.com>
2024-11-19 10:10:13 -0300

    Fix command that disables unused lsps to work with Deno

--- .config/nvim/lua/d-gubert/commands.lua
+++ .config/nvim/lua/d-gubert/commands.lua
@@ -8,15 +8,15 @@ vim.api.nvim_create_user_command('DisableEslintDiagnostics', function()
 	for id, namespace in pairs(vim.diagnostic.get_namespaces()) do
 		-- for some reason this is the name of the `eslint` namespace
 		if namespace.name == 'NULL_LS_SOURCE_3' then
-			vim.diagnostic.disable(nil, id)
+			vim.diagnostic.enable(false, { bufnr = nil, ns_id = id })
 		end
 	end
 end, {})
 
 vim.api.nvim_create_user_command('DisableTSServer', function()
-	for _, client in pairs(vim.lsp.get_active_clients()) do
+	for _, client in pairs(vim.lsp.get_clients()) do
 		-- for some reason this is the name of the `tsserver` namespace
-		if client.config.name == 'tsserver' then
+		if client.name == 'ts_ls' then
 			vim.lsp.stop_client(client.id)
 		end
 	end
