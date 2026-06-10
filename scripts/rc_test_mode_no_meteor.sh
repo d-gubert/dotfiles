ff5ecde3c6bc81040c9923e36d38ee8d5a99fb60
Douglas Gubert <douglas.gubert@gmail.com>
2026-06-10 09:09:31 -0300

    update rc test scripts

--- scripts/rc_test_mode_no_meteor.sh
+++ scripts/rc_test_mode_no_meteor.sh
@@ -25,14 +25,17 @@ TEST_MODE=${TEST_MODE:-'true'}
 PORT=${PORT:-3000}
 MONGO_PORT=${MONGO_PORT:-27017}
 MONGO_DB=${MONGO_DB:-'rocketchat'}
-MONGO_URL=${MONGO_URL:-"mongodb://localhost:${MONGO_DB}/rocketchat?directConnection=true"}
+MONGO_URL=${MONGO_URL:-"mongodb://localhost:${MONGO_PORT}/${MONGO_DB}?directConnection=true"}
 
 TEST_MODE=$TEST_MODE \
 MONGO_URL=$MONGO_URL \
 PORT=$PORT \
 ROOT_URL="http://localhost:${PORT}" \
 OVERWRITE_SETTING_Enterprise_License='Uo7Jcr6WW0XYA8ydHd+Sk6pZ9/0V6dIASnyTwvUrNym/zJg2Ma3eYNKkC8osXLCc72y1ahohnWY7/+7IYkvono3GYXQR+IGvYbbrVgNR6OjMahd9P/odHZL1GFTm2qHrEL5Hh/XEOG+YluFeRdWPzCizQlp4zGGOi0+PkQo096TR9NVCLrsErVl2MW1WM6ZM1W5EUJG9pKly4BQnaOTUAlor1im6i8qPTDCKrISZfLiZEWuQKaPW/GE3mRKjQNjDh0CabX1N2S880pRRGoozBYAnp2NmFfrQW0+5ihKisBTIeMbMZ7K5NE5PkYU1nhQDcc+rpDHtwG9Ceg5X0J+oea3UfrPTmDON2aSI0iO22kvL6G7QI3fyrEIvJrMbxcNKxAFeQYgnjisw/b06+chWSG4jG686Fx58XrVS87dFhWL9WoGltsk1dJCntUQvI1sX6zOfpvyg1iWRnHfYDOrwoWlX57XMm29fWineEoqnOOTOVnA/uP+DKEhercQ9Xuo7Cr6zJxpQpwd03e7ODVjiEbTDqlkZE687rmxRCD4Wmu8L86WIl2xSEIajKLX301Ww5mz/FdLqk+Mg32lkW66W3azQKvJ1440NBrYxhpJ+dl9vSFMb3s1+xnz1cYUbjUcq9mARvORcgy5mLwKulmqT6Sq0Uvbv10YCO0TW0beXYW8=' \
+OVERWRITE_SETTING_Log_Level='2' \
 OVERWRITE_SETTING_Show_Setup_Wizard='completed' \
+OVERWRITE_SETTING_Accounts_Password_Policy_Enabled='false' \
+OVERWRITE_SETTING_Accounts_TwoFactorAuthentication_Enabled='false' \
 OVERWRITE_SETTING_Cloud_Url='https://my.staging.cloud.rocket.chat' \
 OVERWRITE_SETTING_Cloud_Billing_Url='https://billing.staging.cloud.rocket.chat' \
 OVERWRITE_INTERNAL_MARKETPLACE_URL='https://marketplace.staging.cloud.rocket.chat' \
