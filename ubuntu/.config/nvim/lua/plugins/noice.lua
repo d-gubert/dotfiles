e31dca8e7a77d70483a8923f660969afb9d6556e
Douglas Gubert <douglas.gubert@gmail.com>
2026-06-15 12:13:14 -0300

    nvim: add ts-expand-hover plugin

---
+++ ubuntu/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,13 @@
+return {
+	"noice.nvim",
+	opts = {
+		presets = {
+			lsp_doc_border = true
+		},
+		lsp = {
+			hover = {
+				enabled = false,
+			}
+		}
+	}
+}
--- ubuntu/.config/nvim/lua/plugins/ts-expand-hover.lua
+++ ubuntu/.config/nvim/lua/plugins/ts-expand-hover.lua
@@ -1,4 +1,26 @@
+-- TypeScript-only mapping to avoid conflicts with other plugins that map K
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	pattern = { "typescript", "typescriptreact" },
+-- 	callback = function(ev)
+-- 		vim.notify('Expandable hover registered')
+-- 		local hover = function ()
+-- 			vim.notify('Expandable hover called');
+-- 			return require("ts_expand_hover").hover()
+-- 		end
+-- 		vim.keymap.set("n", "K", hover, {
+-- 			buffer = ev.buf,
+-- 			desc = "TypeScript expandable hover",
+-- 			remap = true
+-- 		})
+-- 	end,
+-- })
+
 return {
 	"nemanjamalesija/ts-expand-hover.nvim",
 	ft = { "typescript", "typescriptreact" },
+	config = function ()
+		require("ts_expand_hover").setup({
+			keymaps = { hover = '<leader>h' }
+		})
+	end
 }
