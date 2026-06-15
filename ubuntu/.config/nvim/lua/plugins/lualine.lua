dbe9bba976719c4183ff3786f79898abfc8116aa
Douglas Gubert <douglas.gubert@gmail.com>
2026-06-15 12:16:04 -0300

    nvim: force lualine showing the full path for current file

--- ubuntu/.config/nvim/lua/plugins/lualine.lua
+++ ubuntu/.config/nvim/lua/plugins/lualine.lua
@@ -1,8 +1,24 @@
+local icons = LazyVim.config.icons
+
 return {
-    "nvim-lualine/lualine.nvim",
-    opts = {
-        sections = {
-            lualine_z = {},
-        },
-    },
+	"nvim-lualine/lualine.nvim",
+	opts = {
+		sections = {
+			lualine_c = {
+				LazyVim.lualine.root_dir(),
+				{
+					"diagnostics",
+					symbols = {
+						error = icons.diagnostics.Error,
+						warn = icons.diagnostics.Warn,
+						info = icons.diagnostics.Info,
+						hint = icons.diagnostics.Hint,
+					},
+				},
+				{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+				{ LazyVim.lualine.pretty_path({ length = 0 }) },
+			},
+			lualine_z = {},
+		},
+	},
 }
