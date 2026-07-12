e93f032b7231c5de078b8b7985880e9ac8feb20b
Douglas Gubert <douglas.gubert@gmail.com>
2026-07-12 18:15:13 -0300

    i3: config improvements

--- ubuntu/.config/i3/scripts/printscreen.sh
+++ ubuntu/.config/i3/scripts/printscreen.sh
@@ -1,9 +1,11 @@
 #!/bin/zsh
 
+mkdir -p ~/Pictures/Screenshots/
+
 FNAME=~/Pictures/Screenshots/Screenshot-$(date -Iseconds | cut -d'+' -f1).png
-maim -s $FNAME
+maim -s -u $FNAME
 if [ $? -ne 0 ]; then
-    exit 1
+	exit 1
 fi
 
 xclip -selection clipboard -t image/png -i $FNAME
