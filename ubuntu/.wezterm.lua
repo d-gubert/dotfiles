aa310c75268f3eff9c8a2a9a23453c232b773db6
Douglas Gubert <douglas.gubert@gmail.com>
2026-07-27 11:01:14 -0300

    wezterm: disable default keybindings

--- ubuntu/.wezterm.lua
+++ ubuntu/.wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 config.color_scheme = 'Catppuccin Mocha (Gogh)'
 
@@ -7,5 +8,15 @@ config.font = wezterm.font "FiraCode"
 config.font_size = 11
 
 config.enable_tab_bar = false
+
+config.disable_default_key_bindings = true
+
+config.keys = {
+	{ key = 'C', mods = 'CTRL', action = act.CopyTo 'Clipboard' },
+	{ key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
+	{ key = 'P', mods = 'CTRL', action = act.ActivateCommandPalette },
+	{ key = '-', mods = 'CTRL', action = act.DecreaseFontSize },
+	{ key = '=', mods = 'CTRL', action = act.IncreaseFontSize },
+}
 
 return config
